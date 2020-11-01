e2993946654759b8c19c667ec50f942feb3c3240
aswath <aswath.710@gmail.com>
2020-11-01 19:56:40 +0530

    home page done

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT =  process.env.PORT || 3000
+app.use(express.static('public'))
 app.get('/', (req,res) => {
     res.render('home')
 
 })
 //connecting template
+
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine','ejs')
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
