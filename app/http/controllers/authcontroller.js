c689bab8bdc36402367f19e64d68707cdf05d98b
aswath <aswath.710@gmail.com>
2020-11-03 17:17:37 +0530

    mistakes

--- app/http/controllers/authcontroller.js
+++ app/http/controllers/authcontroller.js
@@ -1,5 +1,5 @@
 const { render } = require("ejs")
-
+const User = require('../../models/user')
 function authcontroller(){
     //factory functions
     return {
@@ -8,8 +8,83 @@ function authcontroller(){
                 res.render('auth/login')
 
         },
-        registration(req, res){
+        postLogin(req,res,next){
+            passport.authenticate('local',(err,user,info)=>{
+                if(err){
+                    req.flash('error',info.message)
+                    return next(err)
+                }
+                if(!user){
+                    req.flash('error',info.message)
+                    return res.redirect('/login')
+                }
+                req.logIn(user,(err)=>{
+                    if(err){
+                        req.flash('error',info.message)
+                        return next(err)
+                    }
+                    return res.redirect('/')
+                })
+
+            })(req,res,next)
+
+        },
+        registration(req,res){
             res.render('auth/registration')
+
+        },
+        async postRegistration(req, res) {
+            const { name, email, password }   = req.body
+            // Validate request 
+            if(!name || !email || !password) {
+                req.flash('error', 'All fields are required')
+                req.flash('name', name)
+                req.flash('email', email)
+               return res.redirect('/registration')
+            }
+   
+            // Check if email exists 
+            User.exists({ email: email }, (err, result) => {
+                if(result) {
+                   req.flash('error', 'Email already taken')
+                   req.flash('name', name)
+                   req.flash('email', email) 
+                   return res.redirect('/registration')
+                }
+            })
+   
+            // Hash password 
+            const hashedPassword = await bcrypt.hash(password, 10)
+            // Create a user 
+            const user = new User({
+                name,
+                email,
+                password: hashedPassword
+            })
+   
+            user.save().then((user) => {
+               // Login
+               return res.redirect('/')
+            }).catch(err => {
+               req.flash('error', 'Something went wrong')
+                   return res.redirect('/registration')
+            })
+           },
+           logout(req, res) {
+             req.logout()
+             return res.redirect('/login')  
+
+
+
+     /*   registration(req, res){
+            res.render('auth/registration')
+        },
+        postRegistration(req, res){
+
+            const {firstname,lastname,password,city} = req.body
+
+            console.log(req.body);*/
+            
         }
     }
 
